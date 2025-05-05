7a4782852865ac5b80c3493cc6ff4dac730bdbec
r3lllax <turkevicaleksandr@gmail.com>
2025-05-05 15:36:28 +0300

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+
+
+class AuthController extends Controller
+{
+
+    /**
+     * Registration
+     *
+     * @param RegistrationRequest $request
+     * @return JsonResponse
+     */
+    public function registration(RegistrationRequest $request)
+    {
+        /** @var User $user */
+        $user = User::query()->create($request->validated());
+        return response()->json([
+            'data'=>[
+                'user'=>[
+                    'name'=>$user->fullName(),
+                    'email'=>$user->email,
+                ],
+                'code'=>201,
+                'message'=>"Created"
+            ],
+        ],201);
+
+    }
+
+    public function authorization(AuthRequest $request)
+    {
+        if(auth()->attempt($request->validated())){
+            /** @var User $user */
+            $user = auth()->user();
+            return response()->json([
+                'data'=>[
+                    'user'=>[
+                        'id'=>$user->id,
+                        'name'=>$user->fullName(),
+                        'birth_date'=>$user->birth_date,
+                        'email'=>$user->email,
+                    ]
+                ],
+                'token'=>$user->createToken('api')->plainTextToken,
+            ]);
+        }
+
+        return response()->json([
+            'code'=>401,
+            'message'=>'Login failed',
+        ],401);
+    }
+}
